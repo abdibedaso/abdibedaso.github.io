7701d40f0cf940a3f3791abd25af6d23d3c96656
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-08 21:26:04 -0500

    add module-scope js

---
+++ module-and-closure/script.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    document.getElementById("create-account-btn").onclick = function() {
+        var accountInfoList = [];
+
+        var nametxt = document.getElementById("name").value;
+        var deposittxt = document.getElementById("deposit").value;
+        var textAreatxt = document.getElementById("txtarea");
+
+        var account;
+        (function() {
+            function createAcount(name, deposit) {
+                return {
+                    name,
+                    deposit
+                }
+            }
+            account = createAcount(nametxt, deposittxt);
+        })();
+
+        accountInfoList.push(account);
+
+        for (let i = 0; i < accountInfoList.length; i++) {
+            textAreatxt.value += "Name : " + accountInfoList[i].name + "Deposit : " + accountInfoList[i].deposit + "\n ";
+        }
+
+    }
+
+}
+
+function rudyTimer() {
+
+    (function() {
+        setInterval(function() {
+            alert("Rudy");
+        }, 1000);
+
+    })();
+
+}
