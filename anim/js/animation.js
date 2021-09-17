1d6935756258784d09b62201b672a99fd77679f4
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-17 11:28:11 -0500

    Create Cutom animation

---
+++ anim/js/animation.js
@@ -0,0 +1,38 @@
+var CUSTOM = "\n" +
+    "   o\n" +
+    "=====\n" +
+    "\n" +
+    "     o\n" +
+    "=====\n" +
+    "\n" +
+    "\n" +
+    "       o\n" +
+    "=====\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "         o\n" +
+    "=====\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "           o\n" +
+    "=====\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "             o\n" +
+    "=====\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "                o\n";
+
+var ANIMATIONS = [];
+ANIMATIONS["Custom"] = ANIMATIONS["custom"] = ANIMATIONS["CUSTOM"] = CUSTOM;
